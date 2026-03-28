8ceff7f28a84d43cd9932df1223e199c900e3d56
Tanmai Kota <[tanmai.kota@gmail.com](mailto:tanmai.kota@gmail.com)>
2026-03-28 16:48:18 +0530

    Use Case 1 completed

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,23 @@
+/**
+ * UseCase1HotelBookingApp
+ * Entry point of Hotel Booking System
+ * 
+ * @author KotaTanmai
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println("================================");
+
+        System.out.println("Application Name : Hotel Booking System");
+        System.out.println("Version          : v1.0");
+
+        System.out.println("Application Started Successfully!");
+
+    }
+}
